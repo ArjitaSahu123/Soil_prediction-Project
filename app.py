e96b923356b03cd2a62c08eae7e6cb3fe2951270
Arjita Sahu <147171689+ArjitaSahu123@users.noreply.github.com>
2026-02-13 13:59:58 +0530

    Modify app run configuration to use environment port

--- app.py
+++ app.py
@@ -78,5 +78,9 @@ def index():
                            fertilizer_recommendations=fertilizer_recommendations,
                            crops=label_encoder.classes_)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+True)
